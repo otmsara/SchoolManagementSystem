d265ccadf83df2c8b99ba9495a644684f327f01f
saidael0 <saidaelmrabet00@gmail.com>
2026-01-31 18:08:08 +0100

    Update project on saida branch

--- src/main/java/com/example/demo/controller/WebController.java
+++ src/main/java/com/example/demo/controller/WebController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 
     @GetMapping("/")
-    public String home() {
-        return "redirect:/admin/dashboard";
-    }
+
 
     public String landing() {
         return "landing";
